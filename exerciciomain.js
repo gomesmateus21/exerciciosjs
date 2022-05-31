3db638e123473c635dbf982fe7d2cdfd794bc791
gomesmateus21 <gomesmateus21@gmail.com>
2022-05-31 01:16:00 -0300

    resolucao ex1

--- exerciciomain.js
+++ exerciciomain.js
@@ -3,10 +3,22 @@
 // Escreva um algoritmo que armazene o valor 999 na variável a e o valor 555 na variável b.
 // A seguir (utilizando apenas atribuições entre variáveis) troque os seus conteúdos fazendo com que o valor que está em a passe para b e vice-versa.
 
+function trocaVar(){
+    var a = 999
+    var b = 555
+    var c = a
+    console.log(a,b,c); 
+    a = b
+    b = c
+    console.log(a,b);
+}
+
 
 // 2
 // Exercício de lógica de programação
 // Faça uma função para calcular o dobro de um número qualquer.
+
+
 
 
 
@@ -27,3 +39,7 @@
 // 5
 // Exercício de lógica de programação
 // Considerando que todos os meses tenham 30 dias, calcular o total de dias de n meses.
+
+
+
+trocaVar();
