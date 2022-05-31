3f1b8442dcbe27823582c9a71c35b7603cf2de89
Gustavo Serafim <tioogu@gmail.com>
2022-05-31 00:57:14 -0300

    Adicionados exercicios para serem feitos

---
+++ exerciciomain.js
@@ -0,0 +1,29 @@
+// Exercício de lógica de programação
+// 1
+// Escreva um algoritmo que armazene o valor 999 na variável a e o valor 555 na variável b.
+// A seguir (utilizando apenas atribuições entre variáveis) troque os seus conteúdos fazendo com que o valor que está em a passe para b e vice-versa.
+
+
+// 2
+// Exercício de lógica de programação
+// Faça uma função para calcular o dobro de um número qualquer.
+
+
+
+// 3
+// Exercício de lógica de programação
+// Sabendo que a área quadrada é dada pela multiplicação dos lados, escreva um algoritmo que mostre a área quadrada de um espaço qualquer.
+// Exemplo:
+// A área de 3x9 é igual a 27
+// 3 . 9 = 27
+
+
+// 4
+// Exercício de lógica de programação
+// Escreva um algoritmo para mostrar o sucessor e o antecessor de um número qualquer.
+
+
+
+// 5
+// Exercício de lógica de programação
+// Considerando que todos os meses tenham 30 dias, calcular o total de dias de n meses.
