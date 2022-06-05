5bec0f2fdd2561d9b5e20bc6c0570e245719e953
Gustavo Serafim <tioogu@gmail.com>
2022-06-05 19:19:07 -0300

    Create level2.js

---
+++ level2.js
@@ -0,0 +1,32 @@
+// 1
+// Crie uma função que descubra se um valor é par ou ímpar.
+// Neste exercício nós utilizarmos o operador módulo %.
+// A operação módulo encontra o resto da divisão de um número por outro.
+// Dados dois números a (o dividendo) e b o divisor, a modulo b (a mod b) é o resto da divisão de a por b.
+// Por exemplo, 7 mod 3 seria 1, enquanto 9 mod 3 seria 0.
+// Portanto sua função deve…
+// retornar 1 ou mais caso negativo (é ímpar) e
+// retornar 0 caso positivo (é par)
+
+// 2
+// Crie uma função que descubra se um valor é maior ou menor que 10.
+
+// 3
+// Exercício de lógica de programação
+// Crie uma função que descubra se um valor é positivo ou negativo (considere o valor zero como positivo)
+
+// 4
+// Crie duas funções para conversão de temperaturas.
+// Uma converterá celsius em fahrenheit e a outra fará o inverso.
+// Saiba que 100c é igual a 212f, veja fórmula:
+//    C          F - 32
+// -------  =  ---------
+//    5            9
+
+// 5
+// Utilizando funções, faça um programa que calcule as raízes da equação do 2 grau conforme a fórmula de Bhaskara.
+// Lembrando:
+// ax(2) + bx + c = 0
+// delta = (Δ = b2 - 4.a.c)
+// x1 = ((-b + √Δ)/2a)
+// x2 = ((-b - √Δ)/2a)
