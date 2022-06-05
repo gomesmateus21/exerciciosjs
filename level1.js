b935e3183ef81a538178bfd6efc6d6b698997831
Gustavo Serafim <tioogu@gmail.com>
2022-06-05 18:17:53 -0300

    Create level1.js

---
+++ level1.js
@@ -0,0 +1,34 @@
+// 1
+// Escreva uma função para cada uma das quatro operações matemáticas simples:
+// adição, subtração, multiplicação e dividisão
+
+// 2 
+// Faça um algoritmo que calcule o reajuste de um salário, utilize os seguintes dados:
+// salário  = 1.000,00
+// reajuste = 15%,
+
+// 3
+// O custo de um carro novo ao consumidor é obtido com a seguinte fórmula:
+// custo final = custo de fábrica +
+//               (custo de fábrica * percentual do distribuidor) +
+//               (custo de fábrica * percentual de impostos)
+// Considerando os valores abaixo, faça um programa para calcular o custo de fabricação.
+
+// Custo de fábrica = 10.000,00
+// Percentual do distribuidor = 28%
+// Percentual dos Impostos  = 45%
+
+// 4
+// Faça um programa para calcular o juros simples segundo a fórmula abaixo.
+// J = C.i.n
+// Onde:
+// J = juros,
+// C = capital,
+// i = taxa de empréstimo
+// n = períodos
+// Vamos imaginar o seguinte cenário: um empréstimo de R$ 16.000,00 sobre a taxa de 4% durante 4 meses.
+
+// 5 
+// Faça um algoritmo que calcule o reajuste de um salário, utilize os seguintes dados:
+// salário  = 1.000,00
+// reajuste = 15%
